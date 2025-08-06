e794fc9456a4715b43833e740899424d820537ed
Yaser Qasemi <yaserqasemi599@gmail.com>
2025-08-06 15:57:37 +0430

    responsive of the tablet add hum menu and add some functions 2:07:12

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
 const bar = document.getElementById("bar");
+const close = document.getElementById("close");
 const nav = document.getElementById("navbar");
+const backDrop = document.getElementById("active-close");
+const body = document.body;
+
+const closeSidebarAndBackDrop = () => {
+    console.log("hi i get clicked");
+    nav.classList.remove("active");
+    backDrop.classList.add("back-drop-hide");
+    body.style.overflow = "auto"; // Allow scroll again
+};
+
+if (close) {
+    close.addEventListener("click", () => {
+        nav.classList.remove("active");
+        backDrop.classList.add("back-drop-hide");
+        body.style.overflow = "auto";
+    });
+}
 
 if (bar) {
+    backDrop.classList.add("back-drop-hide");
     bar.addEventListener("click", () => {
         nav.classList.add("active");
-    })
+        backDrop.classList.remove("back-drop-hide");
+        body.style.overflow = "hidden"; // Disable scroll
+    });
 }
+
